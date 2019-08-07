588c53b4d246d174974b5e46e63de05f8e92c2f7
Swetank Poddar <2424088p@student.gla.ac.uk>
2019-08-08 01:28:54 +0530

    Snake Game

---
+++ script.js
@@ -0,0 +1,213 @@
+
+var canvas,ctx,scoreBoard;
+
+
+//// Colors
+var fillColor = "green"; // body color
+var strokeColor = "white"; // spacing color
+var foodColor = "red" // food color
+///
+
+var onePartSize = 10; // one block size
+var snakeSize = 5; // inital size
+var canvasHeight = 150; // canvas height
+var canvasWidth = 300; // canvas width
+var increasedSize = false; // has eatned food?
+
+var snake = []; // the snake
+
+food = {x:-5, y:-5}; // food position (not reachable at start)
+
+var startingX = 50; // starting x point
+var startingY = 50; // starting y point
+
+var direction = 38; //Key code for down (but it goes up lol)
+
+// Initalize the sname from the start point
+for(i = 0; i<snakeSize; i++){
+    snake.push({x:startingX,y:startingY});
+    startingY += onePartSize; 
+}
+
+//Game over function
+function gameOver() {
+    ctx.font = "30px Arial";
+    snake = [];
+    drawSnake();
+    ctx.fillStyle = "black";
+    ctx.fillRect(0,0,canvasWidth,canvasHeight);
+    ctx.fillStyle = "white";
+    ctx.fillText("Game Over", 10, 50);
+    ctx.fillText("Swetank Poddar :D",10,100);
+    clearInterval(moving);
+}
+
+//Draw snake
+function drawSnakePart(snakePart){
+    // If crosses either of the axis transfer the snake on the other size
+    if(snakePart.x >= canvasWidth){
+        snakePart.x = 0; 
+    }else if(snakePart.x < 0 ){
+        snakePart.x = canvasWidth - 10;
+    }
+
+    if(snakePart.y >= canvasHeight){
+        snakePart.y = 0;
+    }else if(snakePart.y < 0){
+        snakePart.y = canvasHeight - 10;
+    }
+    /////////////////////////////
+
+    // Set the colors
+    ctx.fillStyle = fillColor;
+    ctx.strokeStyle = strokeColor;
+    
+    //Draw the snake at this cordinate
+    ctx.fillRect(snakePart.x,snakePart.y, onePartSize,onePartSize);
+    ctx.strokeRect(snakePart.x, snakePart.y, onePartSize, onePartSize);
+}
+
+// Increase the size of the snake
+function increaseSize(){
+
+    lastPart = snake[snakeSize - 1];
+    secondLastPart = snake[snakeSize - 2];
+
+    //Add the new tail at the correct position
+    // if going up
+    if(lastPart.x == secondLastPart.x){
+        if(lastPart.y - secondLastPart.y < 0){
+            // Going down
+            part = {x:currentPosition.x, y:currentPosition.y - onePartSize};
+        }else {
+            //Going Up
+            part = {x:currentPosition.x, y:currentPosition.y + onePartSize};
+        }
+    } else if(lastPart.y == secondLastPart.y){
+        if(lastPart.x - secondLastPart.x < 0){
+            // Going Right
+            part = {x:currentPosition.x - onePartSize, y:currentPosition.y};
+        } else {
+            // Going Left
+            part = {x:currentPosition.x + onePartSize, y:currentPosition.y};
+            
+        }
+    }
+
+    // Insert the new snake tail
+    snake.push(part);
+
+    // Update score
+
+    scoreBoard.innerHTML = snake.length - snakeSize;
+}
+
+function drawSnake() {
+    //check if collided otherwise draw
+    snake.forEach(function(snakePart){
+        var counter = 0
+        snake.forEach(function(otherSnakePart){
+            // check the number of parts which equal to this snake part
+            if(snakePart.x == otherSnakePart.x && snakePart.y == otherSnakePart.y){
+                counter++;
+            }
+        });
+        // check if the food is eatned and mark appropriate flags
+        if(!increasedSize && snakePart.x == food.x && snakePart.y == food.y){
+            ctx.clearRect(food.x,food.y, 10,10);
+            food = {x: -5, y: -5};
+            increasedSize = true;
+        }
+        // if not colided anywhere draw the snake, otherwise game over.
+        if(counter == 1){
+            drawSnakePart(snakePart);
+        } else {
+            gameOver();
+        }        
+    });
+
+    // if food is eatned increase size
+    if(increasedSize){
+        increaseSize();
+        increasedSize = false;
+        console.log("increased size");
+        getNewFood();
+    }
+}
+
+// move the snake according to the key pressed
+window.onkeydown = function(event){
+
+    // Prevent backfires
+    if((direction == 40 && event.keyCode == 38) || (direction == 38 && event.keyCode == 40)){
+        return;
+    }else if( (direction == 37 && event.keyCode == 39) || (direction == 39 && event.keyCode == 37)){
+        return;
+    }
+
+    ////
+    let part = null;
+    currentPosition = snake[0];
+    switch(event.keyCode){
+        case 37:
+            // Left
+            part = {x:currentPosition.x - onePartSize, y:currentPosition.y};
+            direction = 37;
+            break;
+        case 39:
+            // Right
+            part = {x:currentPosition.x + onePartSize, y:currentPosition.y};
+            direction = 39;
+            break;
+        case 38:
+            // Down
+            part = {x:currentPosition.x, y:currentPosition.y - onePartSize};
+            direction = 38;
+            break;
+        case 40:
+            // Up
+            part = {x:currentPosition.x, y:currentPosition.y + onePartSize};
+            direction = 40;
+            break;
+    }
+    if(part != null){
+        snake.unshift(part);
+        lastPart = snake.pop();
+        ctx.clearRect(lastPart.x,lastPart.y, onePartSize,onePartSize);
+        drawSnake();
+    }
+}
+
+// Place new food
+function getNewFood(){
+    // Get new random location on the canvas
+    randomX = Math.ceil(Math.floor(Math.random() * (canvasWidth-10) + 1) / 10) * 10;
+    randomY = Math.ceil(Math.floor(Math.random() * (canvasHeight-10) + 1) / 10) * 10;
+    /////
+    
+    food.x = randomX;
+    food.y = randomY;
+
+    // Set food color
+    ctx.fillStyle = foodColor;
+    // Plcae the food on canvas
+    ctx.fillRect(randomX, randomY, 10,10);
+    
+    console.log("New food placed");
+}
+
+// go one step more in the current direction every 300 miliseconds
+var moving = setInterval(function(){
+    e = $.Event('keydown');
+    e.keyCode= direction; // direction
+    $(canvas).trigger(e);
+},200);
+
+function initializeGame(canvasId, score, initalSnakeSize  = 5){
+    canvas = document.getElementById(canvasId);
+    scoreBoard = document.getElementById(score);
+    snakeSize = initalSnakeSize;
+    ctx = canvas.getContext('2d');
+    drawSnake();
+    getNewFood();
+}
